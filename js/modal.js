0d0fd47482b04be98b460139b6ee399b4814604f
Nausicaa Cresta <nausicaa.cresta@gmail.com>
2023-01-11 18:05:16 +0100

    test_regex_email

--- js/modal.js
+++ js/modal.js
@@ -62,8 +62,8 @@ const confirmationCloseBtn = document.getElementsByClassName('btn-close');
 
 //  Traitement du formulaire
 
-let regexNames= /^[A-ZÀÄÉÈËÏÖÜÇ][A-Za-zàäéèêëïöüç]+$/i;
-let regexEmail = /^([\w-\.]+@(?!gmail.com)(?!yahoo.com)(?!hotmail.com)(?!outlook.com)(?!laposte.net)(?!orange.fr)(?!sfr.fr)(?!bouyges.fr)(?!iclood.com)(?!me.com)(?!mac.com)([\w-]+\.)+[\w -]{2,4}) ?$/;
+let regexNames= /^[A-ZÇÉÈÊËÀÂÎÏÔÙÛ]{1}[a-zçéèêëàâîïôùû]+[-]?[a-zçéèêëàâîïôùû]+$/i;
+let regexEmail = /^([a-z0-9+!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]) ?$/;
 let regexBirthdate = /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/;
 let regexQuantityTounament = /^[0-9]+$/;
 
@@ -104,7 +104,7 @@ if (!lastname.value.match(regexNames) || lastname.value.length < 1 || lastname.v
 
 let emailValidate=false;
 
-if (regexEmail.test(email) || email.value.length < 1 || email.value == null) {
+if (!email.value.match(regexEmail) || email.value.length < 1 || email.value == null) {
     emailError.innerHTML = 'Adresse mail invalide';
     emailError.classList.add("error-message");
     email.classList.add("error-class");
